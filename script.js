0d2f64f40a65c36023cf61f42e9834e44aa21301
Maks <maksly1008@gmal.com>
2026-02-01 00:59:45 +0100

    force netlify to detect form 5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const form = document.querySelector("form");
 form.addEventListener("submit", function () {
-    alert("Thank you! Your order has been submitted.");
+  alert("Thank you! Your order has been submitted.");
 });
